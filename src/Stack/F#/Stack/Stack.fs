d4082c09def88c01affd944167a1cc5de90bf820
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-11-02 20:05:17 +0200

    Add stack implementation on f#

---
+++ src/Stack/F#/Stack/Stack.fs
@@ -0,0 +1,15 @@
+﻿namespace DataStructures
+
+type Stack<'T>() =
+    let mutable _stack : List<'T> = []
+
+    member this.Push value =
+        _stack <- value :: _stack
+
+    member this.Pop =
+        match _stack with
+        | result :: remainder ->
+            _stack <- remainder
+            result
+        | [] -> failwith "Stack is empty"
+

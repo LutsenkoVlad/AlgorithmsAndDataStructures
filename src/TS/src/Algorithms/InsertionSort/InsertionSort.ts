0e21471d441e99cddd47e093081415f4c568d755
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-11-09 19:30:41 +0200

    cosmetic changes

--- src/TS/src/Algorithms/InsertionSort/InsertionSort.ts
+++ src/TS/src/Algorithms/InsertionSort/InsertionSort.ts
@@ -1,11 +1,11 @@
 export function InsertionSort<T>(arr: T[]) {
     for (let i = 1; i < arr.length; i++) {
         let key = arr[i];
-        let index = i - 1;
-        while (index >= 0 && arr[index] > key) {
-            arr[index + 1] = arr[index];
-            index--;
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[index + 1] = key;
+        arr[j + 1] = key;
     }
 }

989a8c5e95842cb331a9d9badea843e2744dc5ef
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-11-09 16:35:53 +0200

    change implementation insertion sort

--- src/TS/src/Algorithms/InsertionSort/InsertionSort.ts
+++ src/TS/src/Algorithms/InsertionSort/InsertionSort.ts
@@ -1,19 +1,11 @@
 export function InsertionSort<T>(arr: T[]) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > arr[i + 1]) {
-            for (let j = i + 1; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    swap(arr, j - 1, j);
-                } else {
-                    break;
-                }
-            }
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let index = i - 1;
+        while (index >= 0 && arr[index] > key) {
+            arr[index + 1] = arr[index];
+            index--;
         }
+        arr[index + 1] = key;
     }
-}
-
-function swap<T>(arr: T[], left: number, right: number) {
-    const temp = arr[left];
-    arr[left]  = arr[right];
-    arr[right] = temp;
 }

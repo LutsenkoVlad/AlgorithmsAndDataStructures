0015d757c5a06ed2c50f3d9c8a214bc0f2da1238
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-11-09 22:58:26 +0200

    Add tests to insertion sort

--- src/TS/src/Algorithms/InsertionSort/InsertionSort.test.ts
+++ src/TS/src/Algorithms/InsertionSort/InsertionSort.test.ts
@@ -1,7 +1,7 @@
 import { InsertionSort } from "./InsertionSort";
 
 describe("InsertionSort", () => {
-    it("algorithm should sort correctly", () => {
+    it("algorithm should sort correctly [4, 3, 2, 10, 12, 1, 5, 6]", () => {
         let array = [4, 3, 2, 10, 12, 1, 5, 6];
 
         InsertionSort(array);
@@ -15,5 +15,30 @@ describe("InsertionSort", () => {
         expect(array[6]).toEqual(10);
         expect(array[7]).toEqual(12);
         expect(array.length).toEqual(8);
+    });
+
+    it("algorithm should sort correctly [0, 3]", () => {
+        let array = [0, 3];
+
+        InsertionSort(array);
+
+        expect(array[0]).toEqual(0);
+        expect(array[1]).toEqual(3);
+        expect(array.length).toEqual(2);
+    });
+
+    it("algorithm should sort correctly [0, 3]", () => {
+        let array = [-20, -3, 20, 0, 15, -12312123, 1];
+
+        InsertionSort(array);
+
+        expect(array[0]).toEqual(-12312123);
+        expect(array[1]).toEqual(-20);
+        expect(array[2]).toEqual(-3);
+        expect(array[3]).toEqual(0);
+        expect(array[4]).toEqual(1);
+        expect(array[5]).toEqual(15);
+        expect(array[6]).toEqual(20);
+        expect(array.length).toEqual(7);
     });
 });

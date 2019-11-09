fd71fb7790d11ef3698ecbac3ca2ff975e6df5be
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-11-09 22:52:47 +0200

    Add tests to rec version of bubble sort

--- src/F#/AlgorithmsTests/BubbleSortTests.fs
+++ src/F#/AlgorithmsTests/BubbleSortTests.fs
@@ -4,7 +4,7 @@ open Xunit
 open Algorithms
 
 [<Fact>]
-let ``Array should be sorted correctly`` () =
+let ``Array should be sorted correctly [| 3; 7; 4; 4; 6; 5; 8 |]`` () =
     let arr = [| 3; 7; 4; 4; 6; 5; 8 |]
 
     let sortedArr = BubbleSort.Sort arr
@@ -18,9 +18,29 @@ let ``Array should be sorted correctly`` () =
     Assert.Equal(8, sortedArr.[6])
     Assert.Equal(7, sortedArr.Length)
 
+[<Fact>]
+let ``Array should be sorted correctly [| 2; 0 |]`` () =
+    let arr = [| 2; 0 |]
+
+    InsertionSort.Sort arr
+
+    Assert.Equal(0, arr.[0])
+    Assert.Equal(2, arr.[1])
+    Assert.Equal(2, arr.Length)
 
 [<Fact>]
-let ``Array should be sorted correctly rec sort`` () =
+let ``Array should be sorted correctly [| 3; 4; 5; |]`` () =
+    let arr = [| 3; 4; 5; |]
+
+    InsertionSort.Sort arr
+
+    Assert.Equal(3, arr.[0])
+    Assert.Equal(4, arr.[1])
+    Assert.Equal(5, arr.[2])
+    Assert.Equal(3, arr.Length)
+
+[<Fact>]
+let ``Array should be sorted correctly [| 3; 7; 4; 4; 6; 5; 8 |] by rec sort`` () =
     let arr = [| 3; 7; 4; 4; 6; 5; 8 |]
 
     let sortedArr = BubbleSort.RecSort arr
@@ -33,3 +53,24 @@ let ``Array should be sorted correctly rec sort`` () =
     Assert.Equal(7, sortedArr.[5])
     Assert.Equal(8, sortedArr.[6])
     Assert.Equal(7, sortedArr.Length)
+
+[<Fact>]
+let ``Array should be sorted correctly [| 2; 0 |] by rec sort`` () =
+    let arr = [| 2; 0 |]
+
+    let sortedArr = BubbleSort.RecSort arr
+
+    Assert.Equal(0, sortedArr.[0])
+    Assert.Equal(2, sortedArr.[1])
+    Assert.Equal(2, sortedArr.Length)
+
+[<Fact>]
+let ``Array should be sorted correctly [| 3; 4; 5; |] by rec sort`` () =
+    let arr = [| 3; 4; 5; |]
+
+    let sortedArr = BubbleSort.RecSort arr
+
+    Assert.Equal(3, sortedArr.[0])
+    Assert.Equal(4, sortedArr.[1])
+    Assert.Equal(5, sortedArr.[2])
+    Assert.Equal(3, sortedArr.Length)

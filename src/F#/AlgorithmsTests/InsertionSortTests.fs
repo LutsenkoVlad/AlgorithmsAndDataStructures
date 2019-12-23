a2a10b59b1e371b882f3469ecccbeba8cdeb817a
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-12-23 20:38:09 +0200

    Add tests to insertion sorting

--- src/F#/AlgorithmsTests/InsertionSortTests.fs
+++ src/F#/AlgorithmsTests/InsertionSortTests.fs
@@ -3,17 +3,38 @@
 open Xunit
 open Algorithms
 
+[<Theory>]
+let ``Array should be sorted correctly [|3; 7; 4; 4; 6; 5; 8|]`` () =
+    let arr = [| 3; 7; 4; 4; 6; 5; 8 |]
+
+    InsertionSort.Sort arr
+
+    Assert.Equal(3, arr.[0])
+    Assert.Equal(4, arr.[1])
+    Assert.Equal(4, arr.[2])
+    Assert.Equal(5, arr.[3])
+    Assert.Equal(6, arr.[4])
+    Assert.Equal(7, arr.[5])
+    Assert.Equal(8, arr.[6])
+    Assert.Equal(7, arr.Length)
+
 [<Fact>]
-let `` Array should be sorted correctly `` () =
-    let array = [| 5; 7; 8; 3; 6; 3; 2; 7 |]
+let ``Array should be sorted correctly [| 2; 0 |]`` () =
+    let arr = [| 2; 0 |]
 
-    InsertionSort.Sort array
+    InsertionSort.Sort arr
 
-    Assert.Equal(2, array.[0])
-    Assert.Equal(3, array.[1])
-    Assert.Equal(3, array.[2])
-    Assert.Equal(5, array.[3])
-    Assert.Equal(6, array.[4])
-    Assert.Equal(7, array.[5])
-    Assert.Equal(7, array.[6])
-    Assert.Equal(8, array.[7])
+    Assert.Equal(0, arr.[0])
+    Assert.Equal(2, arr.[1])
+    Assert.Equal(2, arr.Length)
+
+[<Fact>]
+let ``Array should be sorted correctly [| 3; 4; 5; |]`` () =
+    let arr = [| 3; 4; 5; |]
+
+    InsertionSort.Sort arr
+
+    Assert.Equal(3, arr.[0])
+    Assert.Equal(4, arr.[1])
+    Assert.Equal(5, arr.[2])
+    Assert.Equal(3, arr.Length)
